93713c43411842d3a772f271282b73b6bd0a2ada
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-07 12:42:49 +0530

    updates

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ window.addEventListener('gamepadconnected', event => {
     console.log('Gamepad connected: ')
     console.log(event.gamepad)
 })
+
+window.addEventListener('gamepaddisconnected', event => {
+    console.log('Gamepad disconnected: ')
+    console.log(event.gamepad)
+})
